3b100b8991ad64fcc92c6ef980e34c21bbcb3ce6
Jon Sagara <jonsagara@yahoo.com>
2009-11-03 00:02:03 -0800

    Added a basic unit test

--- Source/WeightedRandom.Tests/WeightedRandomTests.cs
+++ Source/WeightedRandom.Tests/WeightedRandomTests.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Text;
 
 using NUnit.Framework;
+using WeightedRandom;
 
 namespace WeightedRandom.Tests
 {
 	[TestFixture]
 	public class WeightedRandomTests
 	{
+		[Test]
+		public void WeightedRandom_CreateWithTotalWeight100_Succeeds()
+		{
+			var items = new WeightedRandomItem<string>[]
+			{
+				new WeightedRandomItem<string>("A", 100)
+			};
+
+			Assert.DoesNotThrow(() => { new WeightedRandom<string>(items); }, "Total weights must add up to exactly 100");
+		}
 	}
 }
